10d00cdd2c75c50156db91ff2fc9abb12a21a99f
stebvkim <stevekim9020@gmail.com>
2022-12-04 17:31:39 -0500

    removed redundancies, changed router some router links

--- client/components/Account/LogoutForm.vue
+++ client/components/Account/LogoutForm.vue
@@ -16,7 +16,7 @@ export default {
       fields: [],
       content: 'Taking a break? See you later.',
       callback: () => {
-        this.$router.push({name: 'Discover'}); // Goes to Home page after signing out
+        this.$router.push({name: 'Login'}); // Goes to Home page after signing out
         this.$store.commit('alert', {
           message: 'You are now signed out!', status: 'success'
         });
