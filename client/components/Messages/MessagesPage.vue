0dfce56f629ccf402b7ce62e201e5c93ea3f42c0
Bryan <bpyo@mit.edu>
2022-12-04 03:58:16 -0500

    added create and join group functionality

--- client/components/Messages/MessagesPage.vue
+++ client/components/Messages/MessagesPage.vue
@@ -3,15 +3,69 @@
 
 <template>
   <main>
-    <p>Messages Page</p>
+    <section>
+      <div class="groupOptions">
+        <div><CreateGroupForm /></div>
+        <div><JoinGroupForm /></div>
+      </div>
+      
+    </section>
+    <div class="one">Tab for groups</div>
+    <div class="two">Events</div>
+    <div class="three">Live Chat</div>
   </main>
 </template>
 
 <script>
 import Calendar from '@/components/Profile/Calendar.vue';
+import CreateGroupForm from '@/components/Messages/CreateGroupForm.vue';
+import JoinGroupForm from '@/components/Messages/JoinGroupForm.vue';
+import ChangeUsernameForm from '@/components/Account/ChangeUsernameForm.vue';
 export default {
   name: 'MessagesPage',
-  components: { Calendar
-  }
+  components: { 
+    Calendar,
+    ChangeUsernameForm, 
+    CreateGroupForm,
+    JoinGroupForm
+  },
+  // methods: {
+  // },
 };
 </script>
+
+<style scoped>
+.groupButtons{
+  background-color: #ffffff;
+  font-size: 16px;
+  border: 1px solid  black;
+}
+.groupOptions{
+  /* border-radius: 25px;
+  border: 1px solid #111;  */
+  padding: 1rem;
+  display: flex;
+  justify-content: space-evenly;
+  margin-bottom: 14px;
+  position: relative;
+  margin-top: 25px;
+}
+.one
+{
+    background-color:limegreen;    
+    float: left;  
+    width:33%;
+}
+.two
+{
+    background-color:orange;
+    float: left;
+    width:33%;
+}
+.three
+{
+    background-color: hotpink;
+    float: left;
+    width:33%;
+}
+</style>
