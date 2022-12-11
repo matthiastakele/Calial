09a0ead9f88c2ca66aec16c52d0c12591f434fbe
Matthias Takele <mtakele@mit.edu>
2022-12-11 13:12:33 -0500

    updated navbar

--- client/components/Profile/Calendar.vue
+++ client/components/Profile/Calendar.vue
@@ -199,42 +199,16 @@ export default {
 </script>
 
 <style>
-.vuecal__menu,
-.vuecal__cell-events-count {
-  background-color: rgb(153, 153, 255);
-  font-family: system-ui, -apple-system, system-ui, "Helvetica Neue", Helvetica,
-    Arial, sans-serif;
-}
-.vuecal__title-bar {
-  background-color: #e4f5ef;
-  font-family: system-ui, -apple-system, system-ui, "Helvetica Neue", Helvetica,
-    Arial, sans-serif;
-}
-.vuecal__cell-date {
-  font-family: system-ui, -apple-system, system-ui, "Helvetica Neue", Helvetica,
-    Arial, sans-serif;
-}
-.vuecal__cell--today,
-.vuecal__cell--current {
-  background-color: rgba(240, 240, 255, 0.4);
-  font-family: system-ui, -apple-system, system-ui, "Helvetica Neue", Helvetica,
-    Arial, sans-serif;
-}
-.vuecal:not(.vuecal--day-view) .vuecal__cell--selected {
-  background-color: rgba(235, 255, 245, 0.4);
-}
-.vuecal__cell--selected:before {
-  border-color: rgba(66, 185, 131, 0.5);
-}
+.vuecal__menu, .vuecal__cell-events-count {background-color: #42b983;}
+.vuecal__title-bar {background-color: #e4f5ef;}
+.vuecal__cell--today, .vuecal__cell--current {background-color: rgba(240, 240, 255, 0.4);}
+.vuecal:not(.vuecal--day-view) .vuecal__cell--selected {background-color: rgba(235, 255, 245, 0.4);}
+.vuecal__cell--selected:before {border-color: rgba(66, 185, 131, 0.5);}
 /* Cells and buttons get highlighted when an event is dragged over it. */
 .vuecal__cell--highlighted:not(.vuecal__cell--has-splits),
-.vuecal__cell-split--highlighted {
-  background-color: rgba(195, 255, 225, 0.5);
-}
+.vuecal__cell-split--highlighted {background-color: rgba(195, 255, 225, 0.5);}
 .vuecal__arrow.vuecal__arrow--highlighted,
-.vuecal__view-btn.vuecal__view-btn--highlighted {
-  background-color: rgba(136, 236, 191, 0.25);
-}
+.vuecal__view-btn.vuecal__view-btn--highlighted {background-color: rgba(136, 236, 191, 0.25);}
 
 .vuecal__event {
   cursor: pointer;
--- client/components/common/NavBar.vue
+++ client/components/common/NavBar.vue
@@ -5,30 +5,30 @@
 <template>
   <nav>
     <div class="left">
-      <img class = "logo" src="../../public/logo.png">
+      <img class = "logo" src="../../public/calial_icon.png">
       <h1 class="title">
         Calial
       </h1>
     </div>
     <div class="right">
-      <router-link @click.native = "goToHome" class = "page_link" to="/discover">
-        <font-awesome-icon icon="fa-regular fa-compass" size="xl" />
+      <router-link @click.native = "discover" class = "page_link" :style= "{'font-weight': chosen == 'discover' ? 600 : 300}" to="/discover">
+        Discover
       </router-link>
-      <router-link @click.native = "goToHome" class = "page_link" v-if="$store.state.username" to="/messages">
-        <font-awesome-icon icon="fa-regular fa-message" size="xl" />
+      <router-link @click.native = "messages" class = "page_link" :style= "{'font-weight': chosen == 'messages' ? 600 : 300}" v-if="$store.state.username" to="/messages">
+        Messages
       </router-link>
-      <router-link @click.native="goToProfile" class = "page_link"
+      <router-link @click.native ="goToProfile" class = "page_link" :style= "{'font-weight': chosen == 'profile' ? 600 : 300}"
         v-if="$store.state.username"
         to="/profile"
       >
-        <font-awesome-icon icon="fa-regular fa-user" size="xl"/>
+        My Calendar
       </router-link>
       <p v-else></p>
-      <router-link class = "page_link"
+      <router-link @click.native = "account" class = "page_link" :style= "{'font-weight': chosen == 'account' ? 600 : 300}"
         v-if="$store.state.username"
         to="/account"
       >
-      <font-awesome-icon icon="fa-solid fa-gear" size="xl" />
+      Account
       </router-link>
       <router-link class = "page_link"
         v-else
@@ -54,10 +54,25 @@ export default {
   name: "NavBar",
   data() {
     return {
+      test: "",
+      chosen: ""
     };
   },
+  created(){
+    this.chosen = "discover";
+  },
   methods: {
+    async discover(){
+      this.chosen = "discover";
+    },
+    async messages(){
+      this.chosen = "messages";
+    },
+    async account(){
+      this.chosen = "account";
+    },
     async goToProfile(){
+      this.chosen = "profile";
       this.$store.commit('refreshProfileFreets', this.$store.state.username);
       this.$store.commit('refreshLikes', this.$store.state.userId);
       this.$store.commit('updateProfileUsername', this.$store.state.username);
@@ -71,7 +86,7 @@ export default {
 <style scoped>
 nav {
     padding: 1vw 2vw;
-    background-color: rgb(182, 182, 248);
+    background-color: #1a4775;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -79,15 +94,15 @@ nav {
 }
 
 .title {
-    color: rgb(6, 6, 88);
+    color: #FAF9F6;
     font-family: system-ui,-apple-system,system-ui,"Helvetica Neue",Helvetica,Arial,sans-serif;
     font-size: 1.75em;
     margin: 0 5px;
 }
 
 .page_link{
-  color: #353535;
-  font-weight: 600;
+  color: #FAF9F6;
+
   font-size: 1em;
   text-decoration: none;
   font-family: system-ui,-apple-system,system-ui,"Helvetica Neue",Helvetica,Arial,sans-serif;
@@ -97,22 +112,22 @@ img {
     height: 32px;
 }
 
-
 .left {
 	display: flex;
 	align-items: center;
 }
 
 .right {
-    font-size: 20px;
-    display: grid;
-    gap: 16px;
+    font-size: 21px;
+    display: flex;
+    gap: 12px;
     grid-auto-flow: column;
     align-items: center;
 }
 
 .right a {
-    margin-left: 5px;
+    margin-left: 8px;
+    margin-right: 8px;
 }
 
 .alerts {
